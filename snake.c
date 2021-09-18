26e33a0be27b8848ac2c08104044449f33955012
sspirou <stratspirou@gmail.com>
2021-09-18 01:55:59 -0400

    some initial structure. need to read more about update cycles in c, current use of nanosleep is janky

--- snake.c
+++ snake.c
@@ -2,10 +2,78 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+
+#define BOARD_WIDTH 40
+#define BOARD_HEIGHT 25
+
+typedef enum
+{
+    FALSE,
+    TRUE
+} bool;
+
+typedef enum
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+} direction;
+
+struct Snake
+{
+    int x;
+    int y;
+    int length;
+    direction direction;
+};
+
+bool getIsBoarderCoordinate(int x, int y)
+{
+    return (y == 0 || y == BOARD_HEIGHT - 1) || (x == 0 || x == BOARD_WIDTH - 1);
+}
+
+void drawBoard(struct Snake *snake)
+{
+    system("clear");
+    for (int y = 0; y < BOARD_HEIGHT; y++)
+    {
+        for (int x = 0; x < BOARD_WIDTH; x++)
+        {
+            if (getIsBoarderCoordinate(x, y))
+            {
+                write(STDOUT_FILENO, "#", 1);
+            }
+            else
+            {
+                write(STDOUT_FILENO, " ", 1);
+            }
+        }
+        write(STDOUT_FILENO, "\n", 1);
+    }
+}
+
+bool playing = TRUE;
 
 int main(int argc, char *argv[])
 {
-    char* buff = {"hello world - c\n"};
-    write(STDOUT_FILENO, buff, strlen(buff));
+    struct timespec ts;
+    ts.tv_sec = 10 / 1000;
+    ts.tv_nsec = (10 % 1000) * 1000000;
+
+    struct Snake s;
+    s.x = 10;
+    s.y = 10;
+    s.length = 3;
+    s.direction = DOWN;
+
+    while (playing)
+    {
+        drawBoard(&s);
+
+        nanosleep(&ts, &ts);
+    }
+
     _exit(EXIT_SUCCESS);
 }
