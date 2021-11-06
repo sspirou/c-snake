f72270fd4e89b067abb90314a3f6d907a4c6d396
sspirou <stratspirou@gmail.com>
2021-11-06 01:52:56 -0400

    100ms update function achieved

--- snake.c
+++ snake.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
 
 #define BOARD_WIDTH 40
 #define BOARD_HEIGHT 25
+
+static const int updateTime = 100000;  // 100 milliseconds
 
 typedef enum
 {
@@ -28,6 +31,11 @@ struct Snake
     int length;
     direction direction;
 };
+
+////// GLOBALS
+struct Snake s;
+bool playing = TRUE;
+//////
 
 bool getIsBoarderCoordinate(int x, int y)
 {
@@ -54,25 +62,49 @@ void drawBoard(struct Snake *snake)
     }
 }
 
-bool playing = TRUE;
-
-int main(int argc, char *argv[])
+static void update(int signum)  // SIGALRM
 {
-    struct timespec ts;
-    ts.tv_sec = 10 / 1000;
-    ts.tv_nsec = (10 % 1000) * 1000000;
+    drawBoard(&s);
+}
 
-    struct Snake s;
+static void setAlarmHandler(void)
+{
+    struct sigaction act;
+
+    act.sa_handler = update;
+    act.sa_flags = SA_RESTART;
+    sigfillset(&act.sa_mask);
+
+    sigaction(SIGALRM, &act, NULL);
+}
+
+static void setTimer(void)
+{
+    struct itimerval it;
+
+    it.it_interval = (struct timeval) { .tv_usec = updateTime };
+    it.it_value = it.it_interval;
+
+    setitimer(ITIMER_REAL, &it, NULL);
+}
+
+void initializeSnake(){
     s.x = 10;
     s.y = 10;
     s.length = 3;
     s.direction = DOWN;
+}
+
+int main(int argc, char *argv[])
+{
+    setAlarmHandler();
+    setTimer();
+
+    initializeSnake();
 
     while (playing)
     {
-        drawBoard(&s);
-
-        nanosleep(&ts, &ts);
+        //
     }
 
     _exit(EXIT_SUCCESS);
